28a23b3ec71a5d30ab9ce4dfb69965a43f84fdbf
Nickolaus D. Saint <nick@globus.org>
2019-04-01 10:36:10 -0700

    Added unit tests

--- pilot/schemas/__init__.py
+++ pilot/schemas/__init__.py
@@ -3,10 +3,11 @@ import json
 
 BASE_DIR = os.path.dirname(__file__)
 
+
 def get_schemas():
     schemas = {}
     files = [f for f in os.listdir(BASE_DIR)
-             if os.path.splitext(f)[1] == '.json' ]
+             if os.path.splitext(f)[1] == '.json']
 
     for f in files:
         with open(os.path.join(BASE_DIR, f)) as fh:
---
+++ tests/unit/conftest.py
@@ -0,0 +1,46 @@
+import pytest
+import copy
+from unittest.mock import Mock
+from .mocks import MemoryStorage, MOCK_TOKEN_SET
+
+from pilot.client import PilotClient
+import pilot
+
+
+@pytest.fixture
+def mem_storage():
+    return MemoryStorage()
+
+
+@pytest.fixture
+def mock_tokens():
+    return copy.deepcopy(MOCK_TOKEN_SET)
+
+
+@pytest.fixture
+def mock_auth_pilot_cli(monkeypatch):
+    """
+    Returns a mock logged in pilot client. Storage is mocked with a custom
+    object, so this does behave slightly differently than the real client.
+    All methods that reach out to remote resources are mocked, you need to
+    re-mock them to return the test data you want.
+    """
+    pc = PilotClient()
+    pc.token_storage = MemoryStorage()
+    pc.token_storage.tokens = MOCK_TOKEN_SET
+    pc.upload = Mock()
+    pc.ingest_entry = Mock()
+    pc.get_search_entry = Mock()
+    pc.ls = Mock()
+    # Sanity. This *should* always return True, but will fail if we update
+    # tokens at a later time.
+    assert pc.is_logged_in()
+    return pc
+
+
+@pytest.fixture
+def mock_command_pilot_cli(mock_auth_pilot_cli, monkeypatch):
+    mock_func = Mock()
+    mock_func.return_value = mock_auth_pilot_cli
+    monkeypatch.setattr(pilot.commands, 'get_pilot_client', mock_func)
+    return mock_auth_pilot_cli
