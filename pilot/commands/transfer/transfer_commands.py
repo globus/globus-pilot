3d96acf1ca32f221e0c550d33d1d6350c09da217
Nickolaus D. Saint <nick@globus.org>
2019-04-15 14:42:13 -0700

    Fixed path issue for uploading files with gcp

--- pilot/commands/transfer/transfer_commands.py
+++ pilot/commands/transfer/transfer_commands.py
@@ -127,7 +127,7 @@ def upload(dataframe, destination, metadata, gcp, update, test, dry_run,
             tc, local_ep, pc.ENDPOINT,
             label='{} Transfer'.format(pc.APP_NAME),
             sync_level='checksum')
-        path = os.path.join(filename, destination)
+        path = pc.get_path(filename, destination, test)
         tdata.add_item(dataframe, path)
         click.echo('Starting Transfer...')
         transfer_result = tc.submit_transfer(tdata)
