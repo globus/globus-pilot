451861d7b6b88a4606ba5d4993bc06ca5938d985
Nickolaus D. Saint <nick@globus.org>
2019-07-02 12:37:57 -0700

    Fixed all edge cases with flags and when arg is a directory

--- pilot/commands/search/delete.py
+++ pilot/commands/search/delete.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import click
 import globus_sdk
@@ -6,46 +7,68 @@ import pilot
 log = logging.getLogger(__name__)
 
 
-@click.command(name='delete', help='Delete a search entry')
+@click.command(name='delete', help='Delete file and search record')
 @click.argument('path', type=click.Path())
 @click.option('--entry-id', default='metadata', help=('Delete a specific entry'
               ' within the search subject, or "null" for a null entry id.'))
 @click.option('--subject', default=False, help=('Delete the entire subject '
               'comprising all of its associated entry ids'))
-@click.option('--test', is_flag=True, default=False)
 @click.option('--dry-run', is_flag=True, default=False,
               help="Show report, but don't actually delete entry/file")
-@click.option('--delete-data', 'delete_data', default=False,
-              help='Output as JSON.')
-@click.option('--yes', is_flag=True)
-def delete_command(path, entry_id, subject, test, dry_run, delete_data, yes):
+@click.option('--data', 'data_only', is_flag=True,
+              help='Delete the data but not the search record', default=False)
+@click.option('--metadata', 'metadata_only', default=False, is_flag=True,
+              help='Delete the search record but not the data')
+def delete_command(path, entry_id, subject, dry_run, data_only,
+                   metadata_only):
     pc = pilot.commands.get_pilot_client()
     if not pc.is_logged_in():
         click.echo('You are not logged in.')
         return
 
-    sub_url = pc.get_subject_url(path)
-    if dry_run:
-        click.secho('Dry Run (No Delete Performed)')
-        click.secho('Search Entry: {}'.format(sub_url))
-        click.secho('File: {}'.format(pc.get_path(path, test)))
-        return
+    is_dir = False
+    path = path.rstrip('/')
 
-    try:
-        import os
+    if not metadata_only:
         dir_list = pc.ls(os.path.dirname(path), extended=True)
-        dirname, basename = os.path.dirname(path), os.path.basename(path)
+        basename = os.path.basename(path)
         log.debug('Checking if {} in {}'.format(basename, dir_list))
 
         if os.path.basename(path) not in dir_list:
             click.secho('No file "{}" exists'.format(path), fg='yellow')
             return
-        # elif di
-        click.echo('Removing data... ', nl=False)
-        pc.delete(path)
-        click.echo('Done. \nRemoving search record...', nl=False)
-        pc.delete_entry(path, entry_id=entry_id, full_subject=subject)
-        click.secho('Done. \n{} has been deleted successfully'.format(path),
+        elif dir_list[basename]['type'] == 'dir':
+            subdir_listing = pc.ls(path)
+            log.debug('Subdir contains {}'.format(subdir_listing))
+            if subdir_listing:
+                click.secho('Directory is not empty, contains: {}'
+                            ''.format(subdir_listing), fg='yellow')
+                return 1
+            is_dir = True
+
+    sub_url = pc.get_subject_url(path)
+    if dry_run:
+        click.secho('Dry Run (No Delete Performed)')
+        if is_dir:
+            click.echo('Deleted directory: {}'.format(path))
+        if not metadata_only:
+            click.echo('Deleted file: {}'.format(path))
+        if not data_only:
+            click.echo('Deleted search entry: {}'.format(sub_url))
+        return
+
+    try:
+        if is_dir:
+            pc.delete(path, recursive=True)
+            click.echo('The Directory {} has been removed'.format(path))
+            return
+        if not metadata_only:
+            click.echo('Removing data... ')
+            pc.delete(path)
+        if not data_only:
+            click.echo('Removing search record...')
+            pc.delete_entry(path, entry_id=entry_id, full_subject=subject)
+        click.secho('{} has been deleted successfully'.format(path),
                     fg='green')
     except globus_sdk.exc.SearchAPIError as se:
         if se.code == 'NotFound.Generic':
--- tests/unit/test_command_delete.py
+++ tests/unit/test_command_delete.py
@@ -4,6 +4,7 @@ from pilot.commands.search.delete import delete_command
 
 def test_delete(mock_cli):
     runner = CliRunner()
+    mock_cli.ls.return_value = {'bar': {'type': 'file'}}
     result = runner.invoke(delete_command, ['foo/bar'])
     assert result.exit_code == 0
     assert mock_cli.delete_entry.called
