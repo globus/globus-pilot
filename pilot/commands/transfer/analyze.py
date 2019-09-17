b6944f0f7d07caf48f3add25d7d9985cf228e821
Nickolaus D. Saint <nick@globus.org>
2019-09-17 08:54:53 -0700

    Fixed analyze to skip directories, can now analyze multiple files

--- pilot/commands/transfer/analyze.py
+++ pilot/commands/transfer/analyze.py
@@ -1,36 +1,38 @@
+import os
 import logging
 import click
-import pilot
-import traceback
 from pilot.analysis import analyze_dataframe, mimetypes
 from pilot.exc import AnalysisException
 
 log = logging.getLogger(__name__)
 
+
 @click.command(help='Analyze a dataframe and print the output')
-@click.argument('dataframe',
-                type=click.Path(exists=True, file_okay=True, dir_okay=False,
-                                readable=True, resolve_path=True),)
-def analyze(dataframe):
+@click.argument('dataframes',
+                type=click.Path(exists=True, file_okay=True, dir_okay=True,
+                                readable=True, resolve_path=True),
+                nargs=-1)
+def analyze(dataframes):
     """
     Run analytics on a dataframe and report the output without any other
     action. This is handy if the user isn't sure this will work, and may want
     to pass the --no-analyze flag to upload. Alternatively, they may simply
     want to better understand the contents of a dataframe.
     """
-    pc = pilot.commands.get_pilot_client()
-    if not pc.is_logged_in():
-        click.echo('You are not logged in.')
-        return
-
-    try:
-        mimetype = mimetypes.detect_type(dataframe)
-        log.debug(mimetype)
-        click.secho('Analyzing {}'.format(dataframe))
-        analysis = analyze_dataframe(dataframe, mimetype)
-        from pprint import pprint
-        pprint(analysis)
-    except AnalysisException as ae:
-        click.secho('Error analyzing {}, skipping...'.format(dataframe),
-                    fg='yellow')
-        traceback.print_exception(*ae.original_exc_info)
+    for dataframe in dataframes:
+        basename = os.path.basename(dataframe)
+        if os.path.isdir(dataframe):
+            click.secho('"{}" is a directory, skipping....'.format(basename))
+            continue
+        try:
+            mimetype = mimetypes.detect_type(dataframe)
+            click.secho('Analyzing {}'.format(basename))
+            analysis = analyze_dataframe(dataframe, mimetype)
+            if not analysis:
+                click.secho('Unable to analyze {}'.format(basename))
+                continue
+            from pprint import pprint
+            pprint(analysis)
+        except AnalysisException as ae:
+            click.secho('Error analyzing {}: {}'.format(basename, ae),
+                        fg='yellow')
