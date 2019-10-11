864e5f4034f4133dcd9053a645bcbfa6bc227b1d
Nickolaus D. Saint <nick@globus.org>
2019-10-11 11:56:00 -0700

    Updated filenames for xpcs plots

--- scripts/xpcs_plots.py
+++ scripts/xpcs_plots.py
@@ -9,6 +9,7 @@ from matplotlib.colors import LogNorm
 import pylab
 import h5py
 
+
 def gen_image(dset, basename, cbar=True, log=False):
     if sys.platform == 'darwin':
         matplotlib.use("macOSX")
@@ -35,36 +36,36 @@ def gen_image(dset, basename, cbar=True, log=False):
         plt.colorbar(im, cax=cax)
     plt.savefig(image_filename, dpi=100)
 
+
 def plot_pixelSum(xpcs_h5file):
-    h5fname = xpcs_h5file.filename
     pixelSum_dset = xpcs_h5file['exchange/pixelSum']
-    basename = h5fname.rstrip('.hdf') + '_pixelSum'
+    basename = 'scattering_pattern'
     gen_image(pixelSum_dset, basename)
     gen_image(pixelSum_dset, basename, log=True)
     gen_image(pixelSum_dset, basename + '_pre', cbar=False)
     gen_image(pixelSum_dset, basename + '_pre', cbar=False, log=True)
 
+
 def plot_intensity_vs_time(xpcs_h5file):
-    basename = xpcs_h5file.filename.rstrip('.hdf')
     i_vs_t = xpcs_h5file['exchange/frameSum']
     pylab.plot(i_vs_t[0], i_vs_t[1])
     plt.xlabel("Elapsed Time (s)")
     plt.ylabel("Average Intensity (photons/pixel/frame)")
-    plt.title(basename)
-    plt.savefig(basename + '_frameSum.png')
+    plt.title(xpcs_h5file.filename.rstrip('.hdf'))
+    plt.savefig('total_intensity_vs_time.png')
+
 
 def plot_intensity_vs_q(xpcs_h5file):
-    basename = xpcs_h5file.filename.rstrip('.hdf')
     q = xpcs_h5file['/xpcs/sqlist']
     pmt = xpcs_h5file['/exchange/partition-mean-total']
     pylab.loglog(q[0], pmt[0])
     plt.xlabel("q (A^-1)")
     plt.ylabel("Intensity (photons/pixel/frame)")
-    plt.title(basename)
-    plt.savefig(basename + '_intensity.png')
-    
+    plt.title(xpcs_h5file.filename.rstrip('.hdf'))
+    plt.savefig('intensity_vs_q')
+
+
 if __name__ == '__main__':
-    import sys
     h5filename = sys.argv[1]
     print('opening ' + h5filename)
     x_h5_file = h5py.File(h5filename, 'r')
@@ -73,4 +74,3 @@ if __name__ == '__main__':
     plot_intensity_vs_q(x_h5_file)
     pylab.clf()
     plot_pixelSum(x_h5_file)
-
